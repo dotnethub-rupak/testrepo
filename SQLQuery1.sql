e9154648ffc39d77426f02ab59cec5941cdebc1c
rupakrajput5 <41075626+rupakrajput5@users.noreply.github.com>
2019-09-25 16:16:59 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,28 @@
+
+
+
+select * from dbo.Employee
+
+--delete from dbo.Employee where id=10
+
+--create table dbo.Employee_Documents
+--(
+--DocId int primary key identity(1,1),
+--EmpId int not null,
+--DocName nvarchar(250) not null,
+--DocContent varbinary(max),
+--CreatedDate datetime not null,
+--Active int default(1)
+--)
+
+
+
+select * from dbo.Employee
+
+select * from dbo.Employee_Documents
+
+--truncate table dbo.Employee_Documents
+
+
+
+
